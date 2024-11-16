2894144e36e6b1162c5a2dd333addbf46efea139
EngJao89 <eng_joao@outlook.com>
2024-11-15 21:03:55 -0300

    Initial Commit

---
+++ SignosTests/SignosTests.swift
@@ -0,0 +1,17 @@
+//
+//  SignosTests.swift
+//  SignosTests
+//
+//  Created by João Ricardo Martins Ribeiro on 15/11/24.
+//
+
+import Testing
+@testable import Signos
+
+struct SignosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
